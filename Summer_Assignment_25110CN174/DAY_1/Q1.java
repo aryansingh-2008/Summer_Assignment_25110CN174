4fe9f7bdc52e4def8048c18f854f82e7ec793150
aryansingh-2008 <aryansingh221313@gmail.com>
2026-05-28 04:29:24 +0530

    Updated code

--- Summer_Assignment_25110CN174/DAY_1/Q1.java
+++ Summer_Assignment_25110CN174/DAY_1/Q1.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Q1 {
 
-    public static void main(String[] args){
+    public static void main(String[] args ){
         System.out.println("Enter the value of n");
         Scanner Sc = new Scanner(System.in);
 
--- Summer_Assignment_25110CN174/DAY_1/Q2.java
+++ Summer_Assignment_25110CN174/DAY_1/Q2.java
@@ -6,17 +6,17 @@ public class Q2 {
 
         Scanner Sc = new Scanner(System.in);
         
-        System.out.println("Enter the N");
+        System.out.print("Enter the N  :");
 int N = Sc.nextInt();
 
         for(int i=1;i<=10;i++){
       
             int k = N * i;
 
-            System.out.println(k);
+            System.out.println(N + " x " + i +" =" +(k));
 
 
-        }
+        } Sc.close();
     }
     
 }
