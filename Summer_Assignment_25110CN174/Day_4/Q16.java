7d667d1b60dc9488c7088f590c2eb81870412b83
aryansingh-2008 <aryansingh221313@gmail.com>
2026-05-31 08:27:28 +0530

    Updated code

---
+++ Summer_Assignment_25110CN174/Day_4/Q16.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Q16 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter start: ");
+        int start = sc.nextInt();
+
+        System.out.print("Enter end: ");
+        int end = sc.nextInt();
+
+        for (int i = start; i <= end; i++) {
+
+            int num = i;
+            int sum = 0;
+
+            while (num != 0) {
+                int digit = num % 10;
+                sum = sum + digit * digit * digit;
+                num = num / 10;
+            }
+
+            if (sum == i) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
