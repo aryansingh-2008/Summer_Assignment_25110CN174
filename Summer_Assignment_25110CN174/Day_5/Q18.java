e38c8ebce584177204cda4d367f1c8293a7f5dd2
aryansingh-2008 <aryansingh221313@gmail.com>
2026-06-01 06:51:36 +0530

    Updated code

--- Summer_Assignment_25110CN174/Day_5/Q18.java
+++ Summer_Assignment_25110CN174/Day_5/Q18.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Q18 {
-    public static void main(String[] agrs){
+    public static void main(String[] args){
 
 Scanner Sc= new Scanner(System.in);
 
--- Summer_Assignment_25110CN174/Day_5/Q19.java
+++ Summer_Assignment_25110CN174/Day_5/Q19.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Q19 {
 
-    public static void main(String [] agrs) {
+    public static void main(String [] args) {
 
     Scanner Sc = new Scanner(System.in);
 
