781b3aa463cb552eebc2c65830921c1181ba2edc
aryansingh-2008 <aryansingh221313@gmail.com>
2026-06-16 11:49:11 +0530

    Updated codeL

---
+++ Summer_Assignment_25110CN174/Day_20/Q78.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Q78 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of square matrix: ");
+        int n = sc.nextInt();
+
+        int[][] arr = new int[n][n];
+
+        System.out.println("Enter matrix elements:");
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        boolean symmetric = true;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i][j] != arr[j][i]) {
+                    symmetric = false;
+                    break;
+                }
+            }
+        }
+
+        if (symmetric)
+            System.out.println("Symmetric Matrix");
+        else
+            System.out.println("Not Symmetric Matrix");
+    }
+}
